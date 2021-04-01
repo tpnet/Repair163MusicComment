e502630c5d483864990a386642011fe8f7bd79dd
Skyhans <2269721300@qq.com>
2021-04-01 18:06:56 +0800

    去除NeteaseCloudMusicApi接口，使用原生网易云接口，优化界面显示，优化修改逻辑

--- Assets/Scripts/UIPanel163Key.cs
+++ Assets/Scripts/UIPanel163Key.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -12,23 +13,22 @@ public class UIPanel163Key : MonoBehaviour
     public InputField mIpApiAddr;
     public InputField mIpOrigin;
     public InputField mIpResult;
+    public Toggle mTgCover;
+    public Text mTvStatus;
 
     public Button mBtnDecrypt;
     public Button mBtnEncrypt;
     public Button mBtnEdit;
 
-    // TODO 这里输入你的网易云api地址
-    private string Api = "https://xxxxxxxxxxxx.vercel.app/";
-    private string SearchApi = "search?keywords=";
-    // private string DetailApi = "song/detail?ids=";
+    private string mApi = "";
 
+    //解密Comment的Aes密钥
     private string AesKey = "#14ljk_!\\]&0U<\'(";
-    // Start is called before the first frame update
 
-
+    //保存每首歌的信息
     private Dictionary<string, KeyInfo> mMapKeyInfo = new Dictionary<string, KeyInfo>();
 
-    void Start()
+    private void Start()
     {
         mBtnDecrypt.onClick.AddListener(() =>
         {
@@ -44,19 +44,53 @@ public class UIPanel163Key : MonoBehaviour
         });
         mBtnEdit.onClick.AddListener(() =>
         {
-            Api = mIpApiAddr.text.Trim();
+            mApi = mIpApiAddr.text.Trim();
+            if (string.IsNullOrEmpty(mApi))
+            {
+                Debug.LogError("请输入api地址");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(mIpOrigin.text))
+            {
+                Debug.LogError("请输入文件夹地址");
+                return;
+            }
+
+            if (mApi.Substring(mApi.Length - 1).Equals("/"))
+            {
+                mApi = mApi.Substring(0, mApi.Length - 1);
+            }
+
+            mBtnEdit.interactable = false;
             StartCoroutine(EditCommon(mIpOrigin.text));
         });
+
+        mTvStatus.text = "";
     }
 
+    //忽略的数量
+    private int mIgnoreNum = 0;
+    //失败数量
+    private int mFailNum = 0; 
+    //文件总数量
+    private int mAllNum = 0;
+
+    //修改的数量
+    private int mDealNum = 0;
 
     private IEnumerator EditCommon(string dirPath)
     {
+        mIgnoreNum = 0;
+        mAllNum = 0;
+        mFailNum = 0;
+        mDealNum = 0;
         mMapKeyInfo.Clear();
+
         var pathList = Directory.GetFiles(dirPath);
         var startTime = Time.realtimeSinceStartup;
         Debug.Log("开始搜索:");
-
+        mAllNum = pathList.Length;
         for (var i = 0; i < pathList.Length; i++)
         {
             var path = pathList[i];
@@ -64,25 +98,56 @@ public class UIPanel163Key : MonoBehaviour
             {
                 var name = "";
                 var f = File.Create(path);
+
+                if (!mTgCover.isOn && !string.IsNullOrEmpty(f.Tag.Comment))
+                {
+                    //不覆盖
+                    Debug.LogWarning("跳过：" + f.Tag.Title);
+                    mIgnoreNum++;
+                    continue;
+                }
+
                 name = f.Tag.Title;
-         
+
                 if (string.IsNullOrEmpty(name))
                 {
                     name = path.Substring(path.LastIndexOf("/") + 1);
                     name = name.Substring(0, name.IndexOf("."));
                 }
-
-                f.Dispose();
-
+ 
                 if (!string.IsNullOrEmpty(name))
                 {
-                    yield return Search(path, name);
+                    mDealNum++;
+                    yield return Search(path, name, (beanInfo) =>
+                    {
+                        if (beanInfo != null)
+                        {
+                            var keyInfo = GetMatchSongInfo(f.Tag.Title,f.Tag.Performers,beanInfo.songs);
+                            var comment = JsonMapper.ToJson(keyInfo);
+                            f.Tag.Comment = "163 key(Don't modify):" + AesUtils.Encrypt("music:" + comment, AesKey);
+                            f.Save();
+                        }
+                        else
+                        {
+                            mFailNum++;
+                        } 
+                        mTvStatus.text = "正在处理:" + mDealNum + "/" + mAllNum + "，忽略:" + mIgnoreNum + "，失败:" + mFailNum;
+                        f.Dispose();
+                    });
                 }
             }
         }
 
         Debug.Log("搜索完毕,耗时：" + (Time.realtimeSinceStartup - startTime));
+        // EditCommentInfo();
+        mBtnEdit.interactable = true;
+    }
+
+    private void EditCommentInfo()
+    {
+        mTvStatus.text = "正在修改...";
         mIpResult.text = "";
+        mDealNum = 0;
         foreach (var keyValuePair in mMapKeyInfo)
         {
             // var comment = JsonUtility.ToJson(keyValuePair.Value);
@@ -91,56 +156,123 @@ public class UIPanel163Key : MonoBehaviour
             var f = File.Create(keyValuePair.Key);
             f.Tag.Comment = "163 key(Don't modify):" + AesUtils.Encrypt("music:" + comment, AesKey);
             f.Save();
-            mIpResult.text += "修改歌曲：" + keyValuePair.Key + "\n";
+            // mIpResult.text += "修改歌曲：" + keyValuePair.Key + "\n";
+            mDealNum++;
         }
- 
+
+        mTvStatus.text = "修改完毕" + mDealNum + "首mp3，忽略文件" + mIgnoreNum + "个";
     }
 
 
     /// <summary>
-    /// 
+    /// 搜索歌曲信息
     /// </summary>
     /// <param name="path"></param>
     /// <param name="keyword"></param> 
     /// <returns></returns>
-    private IEnumerator Search(string path, string keyword)
+    private IEnumerator Search(string path, string keyword, Action<SearchInfo> actionDone)
     {
-        var url = Api + SearchApi + keyword;
-        Debug.Log("开始请求：" + url );
+        // var url = Api  + SearchApi + keyword + "&type=1";
+        var url = mApi + "?s=" + keyword + "&type=1";
+        Debug.Log("开始请求：" + url);
         var request = UnityWebRequest.Get(url);
         request.timeout = 5;
         yield return request.SendWebRequest();
         if (request.isHttpError || request.isNetworkError)
         {
             Debug.Log("搜索失败:" + request.error);
+            actionDone.Invoke(null);
         }
         else if (request.isDone)
         {
+            // Debug.Log("结果：" + request.downloadHandler.text);
             var searchList = JsonUtility.FromJson<ApiInfo<SearchInfo>>(request.downloadHandler.text);
             if (searchList.code != 200)
             {
                 Debug.LogError("搜索错误，400:" + path);
+                actionDone.Invoke(null);
                 yield break;
             }
 
             if (searchList?.result?.songs != null && searchList.result.songs.Count > 0)
-            { 
-                mMapKeyInfo.Add(path, searchList.result.songs[0].ToKeyInfo());
+            {
+                actionDone.Invoke(searchList.result);
+                // mMapKeyInfo.Add(path, searchList.result.songs[0].ToKeyInfo());
             }
         }
     }
 
-    
+    /// <summary>
+    /// 匹配最适合的歌曲
+    /// </summary>
+    /// <param name="songName"></param>
+    /// <param name="songActor"></param>
+    /// <param name="list"></param>
+    /// <returns></returns>
+    private KeyInfo GetMatchSongInfo(string songName, string[] songActor, List<SongInfo> list)
+    {
+        if (list == null || list.Count == 0)
+        {
+            return null;
+        }
+
+        var info = list[0];
+        foreach (var songInfo in list)
+        {
+            var name = "";
+            var artist = new List<string>();
+            if (songName.Equals(songInfo.name))
+            {
+                name = songInfo.name.Trim();
+                if (songInfo.artists != null)
+                {
+                    foreach (var songInfoArtist in songInfo.artists)
+                    {
+                        artist.Add(songInfoArtist.name);
+                    }
+                }
+            }
+
+            if (name.Equals(songName.Trim()))
+            {
+                
+                foreach (var s in songActor)
+                {
+                    if (!artist.Contains(s.Trim()))
+                    {//作者名字适配规则
+                        goto for1;
+                    }
+                }
+                
+                //都相等
+                info = songInfo;
+            }
+            
+            for1: ;
+        }
+
+        return info.ToKeyInfo();
+    }
+
+
+    /// <summary>
+    /// 解密Comment
+    /// </summary>
+    /// <param name="ori"></param>
     private void DecryptText(string ori)
     {
-        string result = AesUtils.Decrypt(ori, AesKey);
+        var result = AesUtils.Decrypt(ori, AesKey);
         Debug.Log("" + result);
         mIpResult.text = result;
     }
 
+    /// <summary>
+    /// 加密Comment
+    /// </summary>
+    /// <param name="ori"></param>
     private void EncryptText(string ori)
     {
-        string result = AesUtils.Encrypt("music:" + ori, AesKey);
+        var result = AesUtils.Encrypt("music:" + ori, AesKey);
         mIpOrigin.text = result;
     }
 }
